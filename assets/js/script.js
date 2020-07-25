ec6e6334d3408b178eb9003f3e3847bbfdc42654
Tyler Rooker <60779945+tizzle-b-rizzle@users.noreply.github.com>
2020-07-25 19:17:22 +0100

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//get list full of Luke's poems
+function test(t) {
+    if (t == 1) {
+        document.getElementById('body').style.backgroundColor = "red";
+    } else {
+        document.getElementById('body').style.backgroundColor = "blue";
+    }
+}
